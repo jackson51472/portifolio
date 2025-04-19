315336eb44d22e30a5e16a0d0cc1078cbc6490f4
jacksonf <jacksonf@ciandt.com>
2025-04-19 14:01:12 -0300

    Construindo o header e o footer QUASE PRONTO

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,18 +1,29 @@
-'use server'
-import React from 'react';
-
-
+'use client'
+import React, { useState } from 'react';
+import './header.css';
 
 const Header: React.FC = () => {
+    const [activePage, setActivePage] = useState<string>('#about');
+
+    const handleNavClick = (page: string) => {
+        setActivePage(page);
+    };
+
     return (
         <header className="header">
             <div className="container">
-                <h1 className="header-title">Meu Portfólio</h1>
+                <h1 className="header-title">Jackson Faria</h1>
                 <nav className="header-nav">
-                    <ul className="nav-list">
-                        <li className="nav-item"><a href="#about" className="nav-link">Sobre</a></li>
-                        <li className="nav-item"><a href="#projects" className="nav-link">Projetos</a></li>
-                        <li className="nav-item"><a href="#contact" className="nav-link">Contato</a></li>
+                    <ul className="nav-container">
+                        <li className={`nav-item ${activePage === '#about' ? 'active' : ''}`}>
+                            <a href="#about" className="nav-link" onClick={() => handleNavClick('#about')}>Sobre</a>
+                        </li>
+                        <li className={`nav-item ${activePage === '#projects' ? 'active' : ''}`}>
+                            <a href="#projects" className="nav-link" onClick={() => handleNavClick('#projects')}>Projetos</a>
+                        </li>
+                        <li className={`nav-item ${activePage === '#contact' ? 'active' : ''}`}>
+                            <a href="#contact" className="nav-link" onClick={() => handleNavClick('#contact')}>Contato</a>
+                        </li>
                     </ul>
                 </nav>
             </div>
